3a19a19973258b0e6706029887e3c53d9af0940c
Kalingrowo <kalingrowo@gmail.com>
2022-03-10 15:27:49 +0700

    fix: abstraction, changed mechanism from where-raw-substr to where-like

--- src/NotaGenerator.php
+++ src/NotaGenerator.php
@@ -6,8 +6,7 @@ use Illuminate\Support\Facades\DB;
 
 class NotaGenerator
 {
-
-    private $result;
+    private $result, $tableName, $columnName, $counterLength, $date, $prefix, $prefixSeparator;
 
     public function __construct()
     {
@@ -15,68 +14,109 @@ class NotaGenerator
     }
 
     /**
-     * generate nota
+     * set number preset
      * 
      * @param string $tableName
      * @param string $columnName
      * @param int $counterLength digits length of counter, ex : 001 -> 3 digits, 00001 -> 5 digits
-     * @param string|carbon|null $date format: Y/m/d
+     * @param string|null $date format: Y/m/d || Carbon::class
      * 
-     * @return string eg: PRO/2021/12/23/0001, PRO/0001
+     * @return this
      */
     public function generate(
         string $tableName,
         string $columnName,
         int $counterLength,
-        string $date = null
+        $date = null
     ) {
-        $startCounter = ($counterLength + 11) * -1;
-        if ($date) {
-            $date = now()->parse($date)->format('Y/m/d');
-            $lastNota = DB::table($tableName)
-                ->whereRaw('substr(' . $columnName . ', ' . $startCounter . ', 10) = "' . $date . '"')
-                ->latest($columnName)
-                ->first();
-        } else {
-            $lastNota = DB::table($tableName)
-                ->latest($columnName)
-                ->first();
-        }
-
-        if (!is_null($lastNota)) {
-            $lastNota = $lastNota->$columnName;
-            $lastNumber = (int) substr($lastNota, ($counterLength * -1));
-            $newNumber = $lastNumber + 1;
-        } else {
-            $newNumber = 1;
-        }
-
-        if (!$date) {
-            $this->result = str_pad($newNumber, $counterLength, '0', STR_PAD_LEFT);
-        } else {
-            $this->result = $date . '/' . str_pad($newNumber, $counterLength, '0', STR_PAD_LEFT);
-        }
-
+        $this->tableName = $tableName;
+        $this->columnName = $columnName;
+        $this->counterLength = $counterLength;
+        $this->date = $date;
         return $this;
     }
 
     /**
-     * add prefix to nota
+     * add prefix to number
      * 
      * @param string $prefix example: 'PRO', 'ORDERS', 'NOTA', etc
-     * @param string $separator example: '-', '/', '.', etc
+     * @param string $separator default: '-', example: '-', '/', '.', etc
+     * 
+     * @return this
      */
     public function addPrefix(string $prefix, string $separator = '-')
     {
-        $this->result = $prefix . $separator . $this->result;
+        $this->prefix = $prefix;
+        $this->prefixSeparator = $separator;
         return $this;
     }
 
     /**
      * get generated nota
+     * 
+     * @return string eg: PRO/2021/12/23/0001, PRO/0001
      */
     public function getResult()
     {
+        if ($this->date) {
+            $this->generateNumberWithDate();
+        } else {
+            $this->generateNumberWithoutDate();
+        }
+
+        if ($this->prefix) {
+            $this->result = $this->prefix . $this->prefixSeparator . $this->result;
+        }
+
         return $this->result;
+    }
+
+    // generate number with date
+    private function generateNumberWithDate()
+    {
+        $date = now()->parse($this->date)->format('Y/m/d');
+        $prefixNumber = $date;
+        if ($this->prefix) {
+            $prefixNumber = $this->prefix . $this->prefixSeparator . $date;
+        }
+
+        // get increment number
+        $newNumber = $this->getNextNumber($prefixNumber);
+
+        $this->result = $date . '/' . str_pad($newNumber, $this->counterLength, '0', STR_PAD_LEFT);
+    }
+
+    // generate number without date
+    private function generateNumberWithoutDate()
+    {
+        $prefixNumber = null;
+        if ($this->prefix) {
+            $prefixNumber = $this->prefix . $this->prefixSeparator;
+        }
+
+        // get increment number
+        $newNumber = $this->getNextNumber($prefixNumber);
+
+        $this->result = str_pad($newNumber, $this->counterLength, '0', STR_PAD_LEFT);
+    }
+
+    private function getNextNumber(string $prefixNumber)
+    {
+        // get latest number in database
+        $lastNota = DB::table($this->tableName)
+            ->where($this->columnName, 'like', '%' . $prefixNumber . '%')
+            ->latest($this->columnName)
+            ->first();
+
+        // increment the number
+        $newNumber = 1;
+        if ($lastNota) {
+            $columnName = $this->columnName;
+            $lastNota = $lastNota->$columnName;
+            $lastNumber = (int) substr($lastNota, ($this->counterLength * -1));
+            $newNumber = $lastNumber + 1;
+        }
+
+        return $newNumber;
     }
 }
